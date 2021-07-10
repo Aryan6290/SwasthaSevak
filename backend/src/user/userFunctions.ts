025ddb3c8f5250dc7f25590b4845798fe40f2134
sagar_2001 <sagarshaw2001@gmail.com>
2021-07-10 14:57:10 +0530

    updated

--- backend/src/user/userFunctions.ts
+++ backend/src/user/userFunctions.ts
@@ -4,49 +4,98 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { COLLECTIONS } from "../utils/database";
 export class UserFunctions {
-    constructor(private db: Db) { }
+  constructor(private db: Db) {}
 
-    async registerUser(req: Request, res: Response) {
-        try {
-            const { name, photo, phoneNum, address, identificationNumber, password } = req.body;
-            const find = await this.db.collection(COLLECTIONS.USERS).findOne({ phoneNum });
-            if (find) {
-                res.status(400).send({ status: false, message: "Bad request" });
-            }
-            const hashed = await bcrypt.hash(password, 10);
-            const resp = await this.db.collection("users").insertOne({ name, photo, phoneNum, address, identificationNumber, hashed });
-            if (resp.insertedCount > 0) {
-                res.status(200).send({ status: true, message: "successfully inserted", data: resp.insertedId });
-            }
-            else {
-                res.status(400).send({ status: false, message: "Couldn't insert" });
-            }
-        }
-        catch (err) {
-            console.log(err);
-            res.status(500).send({ status: false, message: "Error in Backend" });
-        }
+  async registerUser(req: Request, res: Response) {
+    try {
+      const { name, photo, phoneNum, address, identificationNumber, password } =
+        req.body;
+      const find = await this.db
+        .collection(COLLECTIONS.USERS)
+        .findOne({ phoneNum });
+      if (find) {
+        res.status(400).send({ status: false, message: "Bad request" });
+      }
+      const hashed = await bcrypt.hash(password, 10);
+      const resp = await this.db.collection("users").insertOne({
+        name,
+        photo,
+        phoneNum,
+        address,
+        identificationNumber,
+        hashed,
+      });
+      if (resp.insertedCount > 0) {
+        res.status(200).send({
+          status: true,
+          message: "successfully inserted",
+          data: resp.insertedId,
+        });
+      } else {
+        res.status(400).send({ status: false, message: "Couldn't insert" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ status: false, message: "Error in Backend" });
     }
+  }
+  // Register Distributor Function
+  async registerDistributor(req: Request, res: Response) {
+    try {
+      const { name, photo, phoneNum, address, gstin, password } = req.body;
+      const distributor = await this.db
+        .collection(COLLECTIONS.DISTRIBUTORS)
+        .findOne({ phoneNum });
+      if (distributor) {
+        res.status(400).send({ status: false, message: "Bad Request" });
+      }
+      const hashed = await bcrypt.hash(password, 10);
+      const resp = await this.db.collection("distributors").insertOne({
+        name,
+        photo,
+        phoneNum,
+        address,
+        gstin,
+        hashed,
+      });
 
-    async loginUser(req: Request, res: Response) {
-        try {
-            const { phoneNum, password } = req.body;
-            const find = await this.db.collection(COLLECTIONS.USERS).findOne({ phoneNum });
-            if (!find) {
-                res.status(404).send({ status: false, message: "User not found" });
-                return;
-            }
-            const compare = await bcrypt.compare(password, find.hashed);
-            if (!compare) {
-                res.status(400).send({ status: false, message: "Unauthorized" });
-                return;
-            }
-            const userDetailsToken = jwt.sign(find, "super-duper-secret");
-            res.status(200).send({ status: true, message: "Signed in", data: userDetailsToken });
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ status: false, message: "Error in Backend" });
-        }
-
+      if (resp.insertedCount > 0) {
+        res.status(200).send({
+          status: true,
+          message: "successfully inserted",
+          data: resp.insertedId,
+        });
+      } else {
+        res.status(400).send({ status: false, message: "Couldn't insert" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ status: false, message: "Error in Backend" });
     }
+  }
+
+  async loginUser(req: Request, res: Response) {
+    try {
+      const { phoneNum, password } = req.body;
+      const find = await this.db
+        .collection(COLLECTIONS.USERS)
+        .findOne({ phoneNum });
+      if (!find) {
+        res.status(404).send({ status: false, message: "User not found" });
+        return;
+      }
+      const compare = await bcrypt.compare(password, find.hashed);
+      if (!compare) {
+        res.status(400).send({ status: false, message: "Unauthorized" });
+        return;
+      }
+      const userDetailsToken = jwt.sign(find, "super-duper-secret");
+      res
+        .status(200)
+        .send({ status: true, message: "Signed in", data: userDetailsToken });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ status: false, message: "Error in Backend" });
+    }
+  }
 }
--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -1,25 +1,26 @@
-import { Db, MongoClient } from 'mongodb';
+import { Db, MongoClient } from "mongodb";
 
 export class Database {
-    private url = "mongodb+srv://debadree:9883362850@cluster0.5meyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    private dbname = "codespirants";
-    private client = new MongoClient(this.url);
-    constructor() {
+  private url =
+    "mongodb+srv://debadree:9883362850@cluster0.5meyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  private dbname = "codespirants";
+  private client = new MongoClient(this.url);
+  constructor() {}
+  async connect(): Promise<Db> {
+    try {
+      if (!this.client || !this.client.isConnected()) {
+        this.client = await MongoClient.connect(this.url, {
+          useUnifiedTopology: true,
+        });
+      }
+    } catch (err) {
+      console.log(err);
     }
-    async connect(): Promise<Db> {
-        try {
-            if (!this.client || !this.client.isConnected()) {
-                this.client = await MongoClient.connect(this.url, {
-                    useUnifiedTopology: true,
-                });
-            }
-        } catch (err) {
-            console.log(err);
-        }
-        return this.client.db(this.dbname);
-    }
+    return this.client.db(this.dbname);
+  }
 }
 
 export const COLLECTIONS = {
-    USERS: "users"
-}
+  USERS: "users",
+  DISTRIBUTORS: "distributors",
+};
