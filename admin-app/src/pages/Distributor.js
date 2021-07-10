a4d7eedc5e35189be8494ff1c9e2653bc8290817
sagar_2001 <sagarshaw2001@gmail.com>
2021-07-11 00:48:28 +0530

    admin-app integrated with backend

--- admin-app/src/pages/Distributor.js
+++ admin-app/src/pages/Distributor.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ListComponent from "../components/Details/ListComponent";
-import { hospitalData } from "../Data/hospitalData";
+// import { hospitalData } from "../Data/hospitalData";
 
 const Distributor = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://swastha-sevak-backend.herokuapp.com/api/distributor?status=pending"
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      });
+  }, []);
+
   return (
     <div className="distributor" style={{ width: "100%" }}>
-      <ListComponent heading="Distributor Information" data={hospitalData} />
+      <ListComponent
+        heading="Distributor Information"
+        data={data}
+        category="distributor"
+      />
     </div>
   );
 };
